4c730c89289ac6e02c304e49d42a0df021f2d424
Marshal <marshlib@gmail.com>
2020-10-08 22:42:15 +0200

    js

---
+++ main.js
@@ -0,0 +1,13 @@
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA'
+    }
+}
+
+console.log(person.address.city);
